5b6bf0d05b7f71d944f910ab3012743dbe74a8f9
zzxiaomajia <71450340+zzxiaomajia@users.noreply.github.com>
2021-03-10 16:52:16 -0600

    Create Leetcode94.java
    LC 94

---
+++ Leetcode94.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List <Integer> res = new ArrayList <> ();
+        Stack <TreeNode> stack = new Stack <>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()){
+            while (cur!= null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        return res;
+
+    }
+}
